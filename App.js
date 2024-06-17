06925780bd84ca2d62b6f6d3d2748162493e555c
Pant <pantkanishk@TAGLDP20770.local>
2024-06-17 12:37:21 +0530

    episode-1

---
+++ App.js
@@ -0,0 +1,13 @@
+
+const parent = React.createElement("div",{id: 'parent'},[
+    React.createElement("div",{id: 'child-1'},[
+        React.createElement("h1",{},"This is heading-1"), 
+        React.createElement("h2",{},"This is heading-2")
+    ]),
+    React.createElement("div",{id: 'child-2'},[
+        React.createElement("h1",{},"This is heading-1"), 
+        React.createElement("h2",{},"This is heading-2")
+    ]),
+])
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(parent);
